0c364c521fb653425992a3f81f5b634ab41cefe2
keefertaylor <keefer.t@gmail.com>
2018-11-02 23:57:58 +0000

    Add an empty Xcode project

---
+++ Fornax/Fornax/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fornax
+//
+//  Created by Keefer Taylor on 11/2/18.
+//  Copyright © 2018 Keefer Taylor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
