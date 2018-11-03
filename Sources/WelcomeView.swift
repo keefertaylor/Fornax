042ab220d2cce7a57a398b888cebd2cf08d54ddd
keefertaylor <keefer.t@gmail.com>
2018-11-03 00:57:24 +0000

    Add WelcomeView class

---
+++ Sources/WelcomeView.swift
@@ -0,0 +1,55 @@
+import Foundation
+import UIKit
+
+public protocol WelcomeViewDelegate: class {
+  func welcomeViewDidPressRestoreWallet(_ welcomeView: WelcomeView)
+}
+
+public class WelcomeView: UIView {
+  public weak var delegate: WelcomeViewDelegate?
+  private let restoreWalletButton: UIButton
+
+  override open class var requiresConstraintBasedLayout: Bool {
+    return true
+  }
+
+  public init() {
+    let restoreWalletButton = UIButton(frame: CGRect.zero)
+    self.restoreWalletButton = restoreWalletButton
+
+    super.init(frame: CGRect.zero)
+
+    restoreWalletButton.setTitle("Restore Wallet", for: .normal)
+    restoreWalletButton.addTarget(self,
+                                  action: #selector(restoreWalletButtonTapped),
+                                  for: .touchUpInside)
+    self.addSubview(restoreWalletButton)
+
+    self.applyConstraints()
+  }
+
+  public override convenience init(frame: CGRect) {
+    self.init()
+  }
+
+  public required convenience init(coder: NSCoder) {
+    self.init()
+  }
+
+  private func applyConstraints() {
+    let margin: CGFloat = 30
+    let buttonHeight: CGFloat = 50
+
+    self.restoreWalletButton.leadingAnchor.constraint(equalTo: self.leadingAnchor,
+                                                      constant: margin).isActive = true
+    self.restoreWalletButton.trailingAnchor.constraint(equalTo: self.leadingAnchor,
+                                                       constant: -margin).isActive = true
+    self.restoreWalletButton.widthAnchor.constraint(equalToConstant: buttonHeight).isActive = true
+    self.restoreWalletButton.bottomAnchor.constraint(equalTo: self.bottomAnchor,
+                                                     constant: -margin).isActive = true
+  }
+
+  @objc private func restoreWalletButtonTapped() {
+    self.delegate?.welcomeViewDidPressRestoreWallet(self)
+  }
+}
