3c04e159eeedee8cc4726c8cdc8a9e1e6d7d3759
keefertaylor <keefer.t@gmail.com>
2018-11-11 01:05:04 +0000

    Conform text fields to MDC

--- Sources/TextField.swift
+++ Sources/TextField.swift
@@ -1,5 +1,16 @@
 import Foundation
-import UIKit
+import MaterialComponents
 
-public class TextField: UITextField {
+public class TextField: MDCTextField {
+  private var controller: MDCTextInputControllerOutlined?;
+
+  public override init(frame: CGRect) {
+    super.init(frame: frame)
+
+    self.controller = MDCTextInputControllerOutlined(textInput: self)
+  }
+
+  public required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+  }
 }
